fd39ac35eb2ceb0009810556cb8101f504799555
NancyBolstad <litchacha@gmail.com>
2020-03-23 13:49:11 +0100

    Refactor

--- src/components/CardsList/index.tsx
+++ src/components/CardsList/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Typography from '../Typography';
-import { ButtonExternal } from '../Button';
 import { Result } from '../../types/data.d';
-import { Wrapper, List, Card as Item, CardImage } from './styles';
-import SearchCards from '../SearchCards';
+import { Wrapper } from './styles';
+import SearchCards from './SearchCards';
+import ResultsWrapper from './List';
 
 export interface Props {
   listTitle?: string;
@@ -28,75 +28,7 @@ export const CardsList: React.FunctionComponent<Props> = ({ listTitle, cards }) 
     <Wrapper>
       {!!listTitle && <Typography element="h2" variant="h2" content={listTitle} align="center" />}
       <SearchCards handler={filter} />
-      {isFullList ? (
-        <List>
-          {(cards || []).map((card, index) => (
-            <Item
-              key={`${card.id}-${index}`}
-              href={`/details/${card.id}`}
-              title={`Go to details about card ${card.name}`}
-              aria-label={`Go to details about card ${card.name}`}
-            >
-              <Typography element="h3" variant="h3" content={card.name} bottom={22} top={16} />
-              {!!card.background_image && <CardImage src={card.background_image} alt={card.name} />}
-              {!!card.released && (
-                <Typography
-                  element="span"
-                  variant="h4"
-                  content={`Released: ${card.released}`}
-                  bottom={16}
-                  top={16}
-                />
-              )}
-              {!!card.rating && (
-                <Typography
-                  element="span"
-                  variant="h4"
-                  content={`Rating: ${card.rating}`}
-                  bottom={16}
-                />
-              )}
-              <ButtonExternal href={`/details/${card.id}`} variant="primary" size="medium">
-                View Details
-              </ButtonExternal>
-            </Item>
-          ))}
-        </List>
-      ) : (
-        <List>
-          {(filtedData || []).map((card, index) => (
-            <Item
-              key={`${card.id}-${index}`}
-              href={`/details/${card.id}`}
-              title={`Go to details about card ${card.name}`}
-              aria-label={`Go to details about card ${card.name}`}
-            >
-              <Typography element="h3" variant="h3" content={card.name} bottom={22} top={16} />
-              {!!card.background_image && <CardImage src={card.background_image} alt={card.name} />}
-              {!!card.released && (
-                <Typography
-                  element="span"
-                  variant="h4"
-                  content={`Released: ${card.released}`}
-                  bottom={16}
-                  top={16}
-                />
-              )}
-              {!!card.rating && (
-                <Typography
-                  element="span"
-                  variant="h4"
-                  content={`Rating: ${card.rating}`}
-                  bottom={16}
-                />
-              )}
-              <ButtonExternal href={`/details/${card.id}`} variant="primary" size="medium">
-                View Details
-              </ButtonExternal>
-            </Item>
-          ))}
-        </List>
-      )}
+      <ResultsWrapper cards={isFullList ? cards : filtedData} />
     </Wrapper>
   );
 };
