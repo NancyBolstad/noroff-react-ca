9d7cf7e602dc002394e119f6414fdc08a5c9118f
NancyBolstad <litchacha@gmail.com>
2020-03-26 14:54:34 +0100

    Clean

---
+++ src/components/ThemeWrapper/styles.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+import createFontStyles from '../../util/createFontStyles';
+
+export interface Props {}
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    color: ${props => props.theme.colors.onBackground};
+    ${props => createFontStyles(props.theme.fonts.b1)}
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
