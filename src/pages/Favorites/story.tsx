83f87d778989e1776909273103cd01775d42115f
NancyBolstad <litchacha@gmail.com>
2020-03-26 16:24:16 +0100

    Fixed storybook file name

--- src/pages/Favorites/story.tsx
+++ src/pages/Favorites/story.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Favorites from '.';
 
-storiesOf('Page/favorites', module).add('Default', () => <Favorites />);
+storiesOf('Page/Favorites', module).add('Default', () => <Favorites />);
