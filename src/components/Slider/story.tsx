def305c4c9273df1f97c0c9afd2b4653f89d4a82
NancyBolstad <litchacha@gmail.com>
2020-03-26 20:01:42 +0100

    Removed console.log

--- src/components/Slider/story.tsx
+++ src/components/Slider/story.tsx
@@ -7,13 +7,10 @@ let cards: Result[];
 
 async function getAllCards() {
   const API_BASE_URL: string = 'https://api.rawg.io/api/games';
-  console.log(11111111);
   try {
-    console.log(22222222);
     const response = await fetch(API_BASE_URL);
     const data: Root = await response.json();
     cards = data.results.slice(1, 5);
-    console.log(cards);
 
     return data;
   } catch (err) {
