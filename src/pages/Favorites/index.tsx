6c563d34558d2c1dcd3570dc4e569090a190f823
NancyBolstad <litchacha@gmail.com>
2020-03-26 08:59:58 +0100

    Render newslist when favorites list is empty

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import Heading from '../../components/Heading';
+import HomeContent from '../../components/HomeContent';
 import { Context } from '../../context/GlobalContext';
+import NewsList from '../../components/NewsList';
 
 interface Props {}
 
@@ -10,12 +11,12 @@ export const Favorites: React.FunctionComponent<Props> = () => {
   console.log(favorites);
 
   return (
-    <>
-      <Heading content="Favorites" isPrimaryColor />
+    <HomeContent>
+      {favorites.length < 1 && <NewsList />}
       {(favorites || []).map((element, index) => (
         <h1 key={index}>{element.name}</h1>
       ))}
-    </>
+    </HomeContent>
   );
 };
 
