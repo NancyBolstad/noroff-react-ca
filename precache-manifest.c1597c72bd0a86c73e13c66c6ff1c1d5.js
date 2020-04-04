5d49f1c7ef439040c9a5541715093645b00ed5a7
NancyBolstad <litchacha@gmail.com>
2020-04-04 17:26:54 +0200

    Updates

---
+++ precache-manifest.c1597c72bd0a86c73e13c66c6ff1c1d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7b411303a07e03b89a06dfabf01201",
+    "url": "/noroff-react-ca/index.html"
+  },
+  {
+    "revision": "ff0bff238a83ad78a648",
+    "url": "/noroff-react-ca/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e9266a421c6ce64e7d9d",
+    "url": "/noroff-react-ca/static/js/2.67cfa04f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/noroff-react-ca/static/js/2.67cfa04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac381f5392db41943d5a",
+    "url": "/noroff-react-ca/static/js/3.9107e5df.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/noroff-react-ca/static/js/3.9107e5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12097426be350eecfeec",
+    "url": "/noroff-react-ca/static/js/4.4c70bae7.chunk.js"
+  },
+  {
+    "revision": "2eb1eb3d64e6b15b2bfa",
+    "url": "/noroff-react-ca/static/js/5.9d3e6d13.chunk.js"
+  },
+  {
+    "revision": "ff0bff238a83ad78a648",
+    "url": "/noroff-react-ca/static/js/main.d9fdf419.chunk.js"
+  },
+  {
+    "revision": "8faa4b0db89718cd5d5f",
+    "url": "/noroff-react-ca/static/js/runtime-main.c54cf6bf.js"
+  }
+]);
---
+++ static/js/5.9d3e6d13.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnoroff-react-ca"]=this["webpackJsonpnoroff-react-ca"]||[]).push([[5],{64:function(e,n,t){"use strict";t.r(n),t.d(n,"Success",(function(){return u}));var a=t(0),r=t(4),c=t(16),o=t(27),u=function(){return a.createElement(c.a,null,a.createElement(r.a,{element:"h1",variant:"h1",content:"Thank you for contacting us!",isPrimaryColor:!0}),a.createElement(o.a,null))};n.default=u}}]);
+//# sourceMappingURL=5.9d3e6d13.chunk.js.map
